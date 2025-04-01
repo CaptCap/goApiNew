55658f82124d7345f5b0c612e51e8816f7ce8930
CaptCap <112699343+CaptCap@users.noreply.github.com>
2025-04-01 19:01:46 +0300

    server

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", api)
+	server := &http.Server{
+		Addr: ":8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
