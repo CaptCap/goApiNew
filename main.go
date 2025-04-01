9aa846f1e2aebac835fc8faa051dc8d894a5d599
CaptCap <112699343+CaptCap@users.noreply.github.com>
2025-04-01 18:58:15 +0300

    router

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import "net/http"
 
 func main() {
-
+	http.HandleFunc("/", api)
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
-
+	w.Write([]byte("Hello, my first go api"))
 }
- 
